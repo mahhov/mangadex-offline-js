ee298a6b3d28905d41e61b72a469319d2c9734aa
manuk <mahhov1@gmail.com>
2021-01-28 12:17:45 -0800

    stream expand: - stream to use single value instead of rest/spread operators. - add stream.promise getter - return new stream on invoking `on`

--- src/services/Stream.js
+++ src/services/Stream.js
@@ -1,26 +1,32 @@
 class Stream {
-	constructor(initializer = undefined, ...initialValues ) {
-		this.hasValues = initialValues.length;
-		this.values = initialValues;
+	constructor(initializer = undefined, initialValue = undefined) {
+		this.hasValue = initialValue !== undefined;
+		this.value = initialValue;
 		this.listeners = [];
 		initializer?.(this.add.bind(this));
 	}
 
-	add(...values) {
-		this.hasValues = true;
-		this.values = values;
-		this.listeners.forEach(listener => listener(...values));
+	add(value) {
+		this.hasValue = true;
+		this.value = value;
+		this.listeners.forEach(listener => listener(value));
 	}
 
-	on(listener) {
-		if (this.hasValues)
-			listener(...this.values);
-		this.listeners.push(listener);
+	on(handler) {
+		let nextStream = new Stream(add =>
+			this.listeners.push(value => add(handler(value))));
+		if (this.hasValue)
+			nextStream.add(handler(this.value));
+		return nextStream;
+	}
+
+	get promise() {
+		return new Promise(resolve => this.on(resolve));
 	}
 
 	static mix(...streams) {
 		let mixStream = new Stream();
-		let sharedListener = _ => mixStream.add(...streams.map(stream => stream.values));
+		let sharedListener = _ => mixStream.add(streams.map(stream => stream.value));
 		streams.forEach(stream => stream.on(sharedListener));
 		return mixStream;
 	}
--- src/services/StreamTest.js
+++ src/services/StreamTest.js
@@ -25,30 +25,60 @@ class DummyCall {
 }
 
 let dc = new DummyCall();
-let stream = new Stream();
+let stream, stream1, stream2; // to avoid accidental redeclarations
+
+// add
+stream = new Stream();
 stream.on(dc.func);
 dc.test();
-
 stream.add(5, 6);
-dc.testOne(5, 6);
+dc.testOne(5);
+stream.add([5, 6]);
+dc.testOne([5, 6]);
 stream.add();
-dc.testOne();
+dc.testOne(undefined);
 stream.add(7);
 dc.testOne(7);
 
+// on with existing value
+stream = new Stream();
+stream.add(6);
+stream.add(7);
 stream.on(dc.func);
 dc.testOne(7);
+stream = new Stream();
+stream.add(undefined);
+stream.on(dc.func);
+dc.testOne(undefined);
+stream.add(undefined);
+dc.testOne(undefined);
 
-stream.add(8, 9);
-dc.test([8, 9], [8, 9]);
+// multiple on
+stream = new Stream();
+stream.on(dc.func);
+stream.on(dc.func);
+stream.add(8);
+dc.test([8], [8]);
 
+// chained on
+stream = new Stream();
+stream.on(dc.func);
+stream1 = stream.on(([a, b]) => a + b);
+stream1.on(dc.func);
+stream2 = stream1.on(a => a + 1);
+stream2.on(dc.func);
+stream.add([10, 20]);
+dc.test([[10, 20]], [30], [31]);
+
+// inital values
 stream = new Stream(undefined, 10);
 stream.on(dc.func);
 dc.testOne(10);
 stream = new Stream(undefined, 30, 31, 32);
 stream.on(dc.func);
-dc.testOne(30, 31, 32);
+dc.testOne(30);
 
+// initializer
 stream = new Stream(add => {
 	add(12);
 	add(13);
@@ -56,28 +86,62 @@ stream = new Stream(add => {
 stream.on(dc.func);
 dc.testOne(13);
 
-let stream1 = new Stream();
+// mix
+stream1 = new Stream();
 stream1.add(5, 6);
-let stream2 = new Stream();
+stream2 = new Stream();
 stream = Stream.mix(stream1, stream2);
 stream.on(dc.func);
-dc.testOne([5, 6], []);
+dc.testOne([5, undefined]);
 stream1.add(6);
-dc.testOne([6], []);
+dc.testOne([6, undefined]);
 stream2.add(7);
-dc.testOne([6], [7]);
+dc.testOne([6, 7]);
 
-// async
-let resolve;
-let promise = new Promise(r => resolve = r);
-stream = new Stream(async add => {
-	await promise;
-	add(21);
-	dc.testOne(21);
-	add(22);
-	dc.testOne(22);
-	// console.log('async tested');
-}, 20);
-stream.on(dc.func);
-dc.testOne(20);
-resolve();
+// async initializer
+{
+	let dc = new DummyCall();
+	let stream;
+	let resolve;
+	let promise = new Promise(r => resolve = r);
+	stream = new Stream(async add => {
+		await promise;
+		add(21);
+		dc.testOne(21);
+		add(22);
+		dc.testOne(22);
+		// console.log('async initializer tested');
+	}, 20);
+	stream.on(dc.func);
+	dc.testOne(20);
+	resolve();
+}
+
+// promise
+{
+	let dc = new DummyCall();
+	let stream;
+	stream = new Stream();
+	stream.promise.then(dc.func);
+	dc.test();
+	stream.add(5);
+	stream.add(6);
+	setTimeout(() => {
+		dc.testOne(5);
+		// console.log('async promise tested')
+	}, 0);
+}
+
+// promise already resolved
+{
+	let dc = new DummyCall();
+	let stream;
+	stream = new Stream(undefined, 5);
+	stream.add(6);
+	stream.promise.then(dc.func);
+	dc.test();
+	setTimeout(() => {
+		dc.testOne(6);
+		// console.log('async promise tested')
+	}, 0);
+}
